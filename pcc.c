7402780b91c579d5215991a9a1fcb2077c0b5ba0
Shimpei Otsubo <potsbo@gmail.com>
2018-12-15 22:11:11 +0900

    Add tokenize and error

--- pcc.c
+++ pcc.c
@@ -1,5 +1,7 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum {
   TK_NUM = 256,
@@ -14,6 +16,44 @@ typedef struct {
 
 
 Token tokens[100];
+
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    /* skip spaces */
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    fprintf(stderr, "Failed to tokenize: %s\n", p);
+    exit(1);
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+void error(int i) {
+  fprintf(stderr, "Unexpected token: %s\n", tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
