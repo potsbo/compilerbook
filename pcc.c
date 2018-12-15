953ab2d2669af1677d5906e491ccce89171721aa
Shimpei Otsubo <potsbo@gmail.com>
2018-12-16 01:47:08 +0900

    Use gen

--- pcc.c
+++ pcc.c
@@ -164,41 +164,15 @@ int main(int argc, char **argv) {
   }
 
   tokenize(argv[1]);
+  Node* node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global _main\n");
   printf("_main:\n");
 
-  if (tokens[0].ty != TK_NUM) {
-    error(0);
-  }
-  printf("  mov rax, %d\n", tokens[0].val);
+  gen(node);
 
-  int i = 1;
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error(i);
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
